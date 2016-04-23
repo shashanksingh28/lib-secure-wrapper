bb2e60b73545ff13bf1ac53a893baa8726038444
Shashank <shashanksing28@gmail.com>
2016-04-23 01:35:07 -0700

    more functions added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lib-secure-wrapper
 Small wrapper C library that remembers malloc's with their max-sizes and helps convert vulnerable overflow calls to safer calls
+
+`gcc -shared -fPIC wrapper.c linkedlist.c -o <libsafe.so>
+LD_PRELOAD=./<libsafe.so> <binary>`
--- wrapper.c
+++ wrapper.c
@@ -6,19 +6,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
 #include "linkedlist.h"
 
 void *(*libc_malloc)(size_t);
 void (*libc_free)(void * p);
+// Original overflow functions
+char *(*libc_strcpy)(char * dest, const char * src);
+char *(*libc_strcat)(char * dest, const char * src);
+char *(*libc_gets)(char *s);
 
 // Linked List that stores allocated pointers with their maxSizes
-List* ptrSizes;
+List * ptrSizes;
 
-void* malloc(size_t sz)
+void * malloc(size_t sz)
 {
-    if (libc_malloc == NULL){
-    	libc_malloc = dlsym(RTLD_NEXT, "malloc");
-	}
+    if (libc_malloc == NULL) libc_malloc = dlsym(RTLD_NEXT, "malloc");
 	if (ptrSizes == NULL) ptrSizes = createList();
 
     void *p = libc_malloc(sz);
@@ -28,19 +31,65 @@ void* malloc(size_t sz)
 }
 
 void free(void* p){
-	if (libc_free == NULL){
-		libc_free = dlsym(RTLD_NEXT, "free");
-	}
+	if (libc_free == NULL) libc_free = dlsym(RTLD_NEXT, "free");
 	if (ptrSizes == NULL) ptrSizes = createList();
 
 	Node* removed = removeFromList(ptrSizes, p);
-	libc_free(p);
-	if (removed == NULL){
-		// not found in our list. probably not called via malloc
-		printf("Not found %p\n",p);
-	}
-	else{
+	
+	if (removed != NULL){
+		libc_free(p);
 		printf("Removed %p : %zd\n", removed -> p, removed -> max_sz);
 	}
+	else{
+		printf("Invalid free call for %p, not allocated\n", p);
+	}
 }
+
+char * strcpy(char * dest, const char * src){
+	if (ptrSizes == NULL) ptrSizes = createList();
+	if (libc_strcpy == NULL) libc_strcpy = dlsym(RTLD_NEXT, "strcpy");
+
+	Node* node = findInList(ptrSizes, dest);
+	if (node != NULL){
+		printf("Found %p : %zd, using strncpy instead of strcpy..\n", dest,node -> max_sz );
+		dest = strncpy(dest, src, node -> max_sz - 1);
+		dest[node -> max_sz - 1] = '\0';
+	}
+	else{
+		// not memory of malloc. Can't help yet
+		dest = libc_strcpy(dest, src);
+	}
+	return dest;
+}
+
+char * strcat(char * dest, const char * src){
+	if (ptrSizes == NULL) ptrSizes = createList();
+	if (libc_strcat == NULL) libc_strcat = dlsym(RTLD_NEXT, "strcat");
+
+	Node* node = findInList(ptrSizes, dest);
+	if (node != NULL){
+		size_t curr_sz = strlen(dest);
+		size_t left = node -> max_sz - curr_sz - 1;
+		dest = strncat(dest, src, left);
+	}
+	else{
+		// no memory of malloc. Can't help yet
+		dest = libc_strcat(dest, src);
+	}
+	return dest;
+}
+
+char * gets(char * s){
+	if (ptrSizes == NULL) ptrSizes = createList();
+	if (libc_gets == NULL) libc_gets = dlsym(RTLD_NEXT, "gets");
+	
+	Node* node = findInList(ptrSizes, s);
+	if(node != NULL){
+		return fgets(s, node -> max_sz, stdin);
+	}
+	else{
+		return libc_gets(s);
+	}
+}
+
 
